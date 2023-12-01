1c1fe4e6919c9155acb96687d90405abc0029154
gourab-9 <gourabsingh09@gmail.com>
2023-12-01 19:04:29 +0530

    Data Transformation Added

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 
@@ -10,13 +10,11 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-
-
+    
     return requirements
 
 
--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -5,6 +5,10 @@ from source.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from source.components.data_transformation import DataTransformation
+from source.components.data_transformation import DataTransformationConfig
+
+
 
 
 @dataclass
@@ -42,8 +46,11 @@ class DataIngestion:
 
             )
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
