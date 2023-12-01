8d3b09e751b33cb64cfcabaf45533aaf03a875ca
gourab-9 <gourabsingh09@gmail.com>
2023-12-01 15:12:32 +0530

    Edit in Code

--- source/components/data_ingestion.py
+++ source/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
+from source.exception import CustomException
+from source.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -9,9 +9,9 @@ from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifacts","train.csv")
-    test_data_path: str=os.path.join("artifacts","test.csv")
-    raw_data_path: str=os.path.join("artifacts","data.csv")
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 
 class DataIngestion:
@@ -25,9 +25,9 @@ class DataIngestion:
             df=pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
-            os.makedir(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestionn_config.raw_data_path,index=False,header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train Test Split Initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
